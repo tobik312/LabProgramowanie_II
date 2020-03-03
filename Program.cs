94c77b1bbf663a411b05f38003489108841896e6
Tobiasz <tobik312@gmail.com>
2020-03-03 21:45:49 +0100

    Inicjacja projektu konsolowego .NET C#

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LabProgramowanie_II
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello DotNet and GIT!!");
+        }
+    }
+}
